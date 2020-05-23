da08efd4f54ed5fe12bfe99d76f8492a166e2a7b
M-O-H <mohanishst@gmail.com>
2020-05-23 22:10:21 +0530

    2. React Components

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./components/MainComponent.js";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hey There! Mohanish Tikekar here.</Text>
-    </View>
-  );
+  return <Main />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#0ff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#0ff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
